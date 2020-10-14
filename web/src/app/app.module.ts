e3604251e31de70caf11eb3f9d1b51d9afc2c424
Radek <radoslaw86n@gmail.com>
2020-10-14 11:42:16 +0200

    basic bills form

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BillsComponent } from './components/bills/bills.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BillsComponent} from './components/bills/bills.component';
+import {AppRoutingModule} from './app-routing.module';
 import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -15,9 +16,12 @@ import {RouterModule} from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- web/src/app/components/bills/bills.component.ts
+++ web/src/app/components/bills/bills.component.ts
@@ -1,21 +1,76 @@
 import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-bills',
   templateUrl: './bills.component.html',
   styleUrls: ['./bills.component.css']
 })
-export class BillsComponent implements OnInit {
-  nameVal: string;
 
-  constructor() {
+export class BillsComponent implements OnInit {
+  payersGroup: FormGroup;
+  payerNameInput = ' ';
+
+  constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.payersGroup = this.fb.group({
+      payers: this.fb.array([])
+    });
   }
 
-  // tslint:disable-next-line:typedef
-  showVal(value: string) {
-    this.nameVal = 'hello ' + value;
+  /*
+  *  payers functions
+  * */
+  payers(): FormArray {
+    return this.payersGroup.get('payers') as FormArray;
   }
+
+  removePayer(payIndex: number): void {
+    this.payers().removeAt(payIndex);
+  }
+
+  createPayerFormGroup(name: string): FormGroup {
+    return this.fb.group({
+      payerName: name,
+      bills: this.fb.array([])
+    });
+  }
+
+  /*
+  *
+  *  bills functions
+  * */
+  payerBills(payIndex: number): FormArray {
+    return this.payers().at(payIndex).get('bills') as FormArray;
+  }
+
+  removePayerBill(payIndex: number, billIndex: number): void {
+    this.payerBills(payIndex).removeAt(billIndex);
+  }
+
+  createPayerBillsGroup(bill: string): FormGroup {
+    return this.fb.group({
+      value: bill
+    });
+  }
+
+  /*
+  *
+  *  buttons action
+  * */
+  addPayersButtonClick(name: string): void {
+    this.payers().push(this.createPayerFormGroup(name));
+    this.payerNameInput = '';
+  }
+
+  addBillButtonClick(payIndex: number, bill: string): void {
+    this.payerBills(payIndex).push(this.createPayerBillsGroup(bill));
+  }
+
+  onSubmit($event: any): void {
+    console.log(this.payersGroup.value);
+  }
+
 }
